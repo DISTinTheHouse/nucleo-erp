92ac0e44bb55ea9001c6cd6abf01d2e91c5f66df
Deadlink90 <parracristian717@gmail.com>
2026-02-18 13:56:59 -0600

    CRUD operations for catalogo app

---
+++ catalogo/api/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from catalogo.api.views import TipoProductoViewSet, CategoriaProductoViewSet, ColorViewSet, TallaViewSet, ProductoVarianteViewSet
+
+tipo_producto_router = DefaultRouter()
+tipo_producto_router.register(prefix='tipo-producto', viewset=TipoProductoViewSet, basename='tipo-producto')
+
+categoria_producto_router = DefaultRouter()
+categoria_producto_router.register(prefix='categoria-producto', viewset=CategoriaProductoViewSet, basename='categoria-producto')
+
+color_router = DefaultRouter()
+color_router.register(prefix='color', viewset=ColorViewSet, basename='color')
+
+talla_router = DefaultRouter()
+talla_router.register(prefix='talla', viewset=TallaViewSet, basename='talla')
+
+#TODO: Producto router
+
+producto_variante_router = DefaultRouter()
+producto_variante_router.register(prefix='producto-variante', viewset=ProductoVarianteViewSet, basename='producto-variante')
+
+urlpatterns = [
+    path('', include(tipo_producto_router.urls)),
+    path('', include(categoria_producto_router.urls)),
+    path('', include(color_router.urls)),
+    path('', include(talla_router.urls)),
+    path('', include(producto_variante_router.urls)),
+]
