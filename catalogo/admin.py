f601fe5be5482d886fef0b3f11f41e48d70e2b37
Deadlink90 <parracristian717@gmail.com>
2026-02-28 08:38:53 -0600

    add terceros and ventas apps; update inventarios app; update routes

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from catalogo.models import TipoProducto, CategoriaProducto, Color, Talla, Producto, ProductoVariante
 
-admin.site.register(TipoProducto) 
+admin.site.register(TipoProducto)
 admin.site.register(CategoriaProducto)
 admin.site.register(Color)
 admin.site.register(Talla)
