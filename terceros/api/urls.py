016c29b7dc12b307313cd6df9f3c5793f1769799
Deadlink90 <parracristian717@gmail.com>
2026-03-05 09:35:56 -0600

    Create CRUD for cliente model

--- terceros/api/urls.py
+++ terceros/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from terceros.api.views import ProveedorViewSet
+from terceros.api.views import ProveedorViewSet, ClienteViewSet
 
 router = DefaultRouter()
 router.register(r'proveedores', ProveedorViewSet, basename='proveedor')
+router.register(r'clientes', ClienteViewSet, basename='cliente')
 
 urlpatterns = [
     path('', include(router.urls)),
