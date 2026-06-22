3d231aca305a6bbbf930b547238dedda2b43d6fd
LuisChavezz <ing.luischavezz@gmail.com>
2026-06-22 07:32:09 -0600

    Add tipo_id filter to ProductoViewSet

--- catalogo/api/views.py
+++ catalogo/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 from catalogo.models import TipoProducto, CategoriaProducto, Color, Talla, Producto, ProductoVariante
 from catalogo.api.serializers import TipoProductoSerializer, CategoriaProductoSerializer, ColorSerializer, TallaSerializer, ProductoSerializer, ProductoVarianteSerializer
 
@@ -27,6 +28,17 @@ class TallaViewSet(viewsets.ModelViewSet):
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        tipo_id = self.request.query_params.get('tipo_id')
+        if tipo_id is not None:
+            try:
+                tipo_id = int(tipo_id)
+            except (TypeError, ValueError):
+                raise ValidationError({"tipo_id": "Must be an integer."})
+            qs = qs.filter(tipo_id=tipo_id)
+        return qs
 
     def perform_create(self, serializer):
         user = self.request.user
